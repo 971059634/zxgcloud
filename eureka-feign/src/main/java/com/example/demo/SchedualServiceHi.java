42cb96a4d7056003dcc53e2d66ada58cdecab7eb
张晓光 <971059634@qq.com>
2017-12-19 15:49:13 +0800

    Initial commit

---
+++ eureka-feign/src/main/java/com/example/demo/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * company :天成科技
+ * Created by zxg on 2017/12/18
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
